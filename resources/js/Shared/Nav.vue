46c5e20f2fee00333996f69182e1a458beb816e8
Ameerali C <ameeralic95@gmail.com>
2023-02-03 17:49:07 +0530

    merge with laracast

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,11 +1,17 @@
 <template>
-    <Link href="/home">home</Link>
+  <nav>
+    <ul>
+      <li><Link href="/">Home</Link></li>
+      <li><Link href="/users">Users</Link></li>
+      <li><Link href="/settings">Settings</Link></li>
+    </ul>
+  </nav>
 </template>
+
 <script>
-import { Link } from "@inertiajs/vue3";
+import { Link } from '@inertiajs/inertia-vue3';
+
 export default {
-    components: {
-        Link,
-    },
+  components: { Link },
 };
 </script>
