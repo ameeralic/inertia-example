afaf845cfab84fb966da8c9f634c5ecd3089ddd2
Ameerali C <ameeralic@Ameeralis-MacBook-Air.local>
2023-02-05 21:34:41 +0530

    pagination

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -2,9 +2,37 @@
   <Head title="Users" />
     <h1 class="text-3xl">Users</h1>
 
-  <ul>
-    <li v-for="user in users" :key="user" v-text="user.name"></li>
-  </ul>
+    <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="user in users.data" :key="user.id">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div>
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ user.name }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <Link :href="`/users/${user.id}/edit`" class="text-indigo-600 hover:text-indigo-900">
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <Pagination :links="users.links" class="mt-6" />
 
     <!-- time and refresh button - showing prevent scrolling on load -->
     <!-- <div style="margin-top: 400px">
@@ -24,6 +52,7 @@
 </template>
 
 <script setup>
-defineProps(['users'])
+import Pagination from "../Shared/Pagination.vue";
+defineProps({ users: Object });
 // defineProps(['time'])
 </script>
