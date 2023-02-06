4ad3df86644275fc616748fd0b9c4ed7c624cde0
Ameerali C <ameeralic95@gmail.com>
2023-02-06 17:09:57 +0530

    for error handling

--- resources/js/Pages/Users/Create.vue
+++ resources/js/Pages/Users/Create.vue
@@ -4,54 +4,100 @@
     <h1 class="text-3xl">Create New User</h1>
 
     <form @submit.prevent="submit" class="max-w-md mx-auto mt-8">
-    <div class="mb-6">
-      <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="name"> Name </label>
+        <div class="mb-6">
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                for="name"
+            >
+                Name
+            </label>
 
-      <input v-model="form.name" class="border border-gray-400 p-2 w-full" type="text" name="name" id="name" required />
-    </div>
+            <input
+                v-model="form.name"
+                class="border border-gray-400 p-2 w-full"
+                type="text"
+                name="name"
+                id="name"
+                required
+            />
+            <div
+                v-if="errors.name"
+                v-text="errors.name"
+                class="text-red-500 text-xs mt-1"
+            ></div>
+        </div>
 
-    <div class="mb-6">
-      <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email"> Email </label>
+        <div class="mb-6">
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                for="email"
+            >
+                Email
+            </label>
 
-      <input
-        v-model="form.email"
-        class="border border-gray-400 p-2 w-full"
-        type="email"
-        name="email"
-        id="email"
-        required
-      />
-    </div>
+            <input
+                v-model="form.email"
+                class="border border-gray-400 p-2 w-full"
+                type="email"
+                name="email"
+                id="email"
+                required
+            />
 
-    <div class="mb-6">
-      <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password"> Password </label>
+            <div
+                v-if="errors.email"
+                v-text="errors.email"
+                class="text-red-500 text-xs mt-1"
+            ></div>
+        </div>
 
-      <input
-        v-model="form.password"
-        class="border border-gray-400 p-2 w-full"
-        type="password"
-        name="password"
-        id="password"
-        required
-      />
-    </div>
+        <div class="mb-6">
+            <label
+                class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                for="password"
+            >
+                Password
+            </label>
 
-    <div class="mb-6">
-      <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">Submit</button>
-    </div>
-  </form>
+            <input
+                v-model="form.password"
+                class="border border-gray-400 p-2 w-full"
+                type="password"
+                name="password"
+                id="password"
+                required
+            />
+            <div
+                v-if="errors.password"
+                v-text="errors.password"
+                class="text-red-500 text-xs mt-1"
+            ></div>
+        </div>
 
-    
+        <div class="mb-6">
+            <button
+                type="submit"
+                class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500"
+            >
+                Submit
+            </button>
+        </div>
+    </form>
 </template>
 <script setup>
-import {router} from '@inertiajs/vue3';
-import { reactive } from 'vue';
+import { router } from "@inertiajs/vue3";
+import { reactive } from "vue";
+
+defineProps({
+    errors: Object,
+});
+
 let form = reactive({
-    name:'',
-    email:'',
-    password:''
-})
-let submit = () =>{
-    router.post('/users', form);
-}
+    name: "",
+    email: "",
+    password: "",
+});
+let submit = () => {
+    router.post("/users", form);
+};
 </script>
